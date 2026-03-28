72b958ff87497e9d401fb061e26ddfe515bd1726
tecnommerce <tecnocommercee@gmail.com>
2026-03-28 18:16:39 -0300

    Agregar panel administrativo global con CORS corregido

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ===================================================
 
 // Reemplazá con la URL que copiaste de Google Apps Script
-const API_URL = 'https://script.google.com/macros/s/AKfycbzi094IjVZVpPMmyJeXVek7HAkXVQ0QANmyhJHNw11b76gViIXtXDr66PwKNhLKmiS1/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzNvzxNU5KGH1Aj48qX5KoQf1MG__n5DFn-Ga9MWB9asb7aS9fwwYjsvcC2bTlfMBNH/exec';
 
 window.WANT_CONFIG = {
     apiUrl: API_URL
