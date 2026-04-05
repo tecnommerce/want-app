e8f0471ed9699df801457813276485d17bc7bbd9
tecnommerce <tecnocommercee@gmail.com>
2026-04-05 04:34:55 -0300

    Integrando supabase

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@
 
 // ⚠️ REEMPLAZA ESTOS VALORES CON LOS TUYOS ⚠️
 const SUPABASE_URL = 'https://owrpzmgncfrgatzccjlc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnB6bWduY2ZyZ2F0emNjamxjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTM2MjE1MiwiZXhwIjoyMDkwOTM4MTUyfQ.oezxPXhfbVpJp05n2SS6Y0X9Ukr-aW8k9m6jFncVrxo';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnB6bWduY2ZyZ2F0emNjamxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNjIxNTIsImV4cCI6MjA5MDkzODE1Mn0.TOFcl0_Ua3jpISBKnrNdI4skIFMiyitWv0rLDoTzTkQ';
 
 // Inicializar Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
