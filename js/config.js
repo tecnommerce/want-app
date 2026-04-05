2a0f4c2657e1c3e2abb6c4a0bf10176297eee846
tecnommerce <tecnocommercee@gmail.com>
2026-04-05 04:32:52 -0300

    Integrando supabase

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 // Puedes encontrarlos en: Project Settings → API
 
 const SUPABASE_URL = 'https://owrpzmgncfrgatzccjlc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnB6bWduY2ZyZ2F0emNjamxjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTM2MjE1MiwiZXhwIjoyMDkwOTM4MTUyfQ.oezxPXhfbVpJp05n2SS6Y0X9Ukr-aW8k9m6jFncVrxo';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnB6bWduY2ZyZ2F0emNjamxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNjIxNTIsImV4cCI6MjA5MDkzODE1Mn0.TOFcl0_Ua3jpISBKnrNdI4skIFMiyitWv0rLDoTzTkQ';
 
 // ===================================================
 // CLOUDINARY CONFIGURACIÓN (NO CAMBIA)
